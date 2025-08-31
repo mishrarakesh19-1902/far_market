86eca997c7177096e7b3e2a624a475b2ebb77073
mishrarakesh19-1902 <rakeshkumarmishra1902@gmail.com>
2025-08-31 14:07:32 +0530

    Update settings.py

--- farm_market/settings.py
+++ farm_market/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['farm-market-project-1-j6fq.onrender.com', 'localhost', '127.0.0.1']
 
